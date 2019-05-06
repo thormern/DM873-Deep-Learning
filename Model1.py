28ab5b0a5cf03d7bc009d603a1dac79e8dddb317
Thor <tes@esdhkonsulenterne.dk>
2019-05-06 21:44:40 +0200

    Thor --- Opsat en test model, mest til undersøgelse af keras.

--- Model1.py
+++ Model1.py
@@ -6,6 +6,7 @@ from keras.layers import Activation, Dropout, Flatten, Dense, Input, Concatenate
 from keras import backend as K
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
+from keras import layers
 
 trainImageDataGen = ImageDataGenerator(rescale=1/255.,horizontal_flip=True)
 validationImageDataGen = ImageDataGenerator(rescale=1/255.)
@@ -21,6 +22,36 @@ valGen = validationImageDataGen.flow_from_directory(
 	target_size=(224,224),
 	batch_size=16,
 	class_mode="binary")
+
+
+#Test Model in the making - Thor
+def create_model( x_train ):
+
+	model = keras.Sequential()
+
+	keras.layers.Conv1D(filters, kernel_size, strides=1, padding='valid', data_format='channels_last', dilation_rate=1,
+						activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros',
+						kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+						kernel_constraint=None, bias_constraint=None)
+	keras.layers.Dense(units, activation=None, use_bias=True, kernel_initializer='glorot_uniform',
+					   bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None,
+					   activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+
+	keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)
+	keras.layers.Activation(activation)
+	keras.layers.Dropout(rate, noise_shape=None, seed=None)
+	keras.layers.Flatten(data_format=None)
+	optimiser = Adam( lr = 1e-3 )  #hmmm
+
+	model.compile( optimizer = optimiser,
+				   loss = 'binary_crossentropy',
+				   metrics =[ 'accuracy'] )
+
+	return model
+
+
+
+
 
 
 #Test af git
