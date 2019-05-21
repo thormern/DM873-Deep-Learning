658858cdd10cbe0bc8f5c1e0c9c46e8ef42de9f0
Thor <tes@esdhkonsulenterne.dk>
2019-05-21 13:48:41 +0200

    Thor --- Functional Model, New Model, must be tested on correct dataset

--- model/model1_functional.py
+++ model/model1_functional.py
@@ -43,7 +43,8 @@ valGen = validationImageDataGen.flow_from_directory(
 
 
 
-
+# ShitOnTheFloorGottaGetSchwiftyInHereModel
+'''
 inputLayer = Input(shape=(IMG_SIZE,IMG_SIZE,3))
 
 conv2D_1 = Convolution2D(32, kernel_size=(1,1), activation='relu')(inputLayer)
@@ -52,28 +53,69 @@ conv2D_3 = Convolution2D(32, kernel_size=(1,1), activation='relu')(inputLayer)
 maxPool_1 = MaxPooling2D(pool_size=(3,3))(inputLayer)
 
 conv2D_4 = Convolution2D(32, kernel_size=(3,3), activation='relu')(conv2D_2)
-conv2D_5 = Convolution2D(32, kernel_size=(5,5), activation='relu')(conv2D_3)
+conv2D_5 = Convolution2D(32, kernel_size=(7,1), activation='relu')(conv2D_3)
+conv2D_5_2 = Convolution2D(32, kernel_size=(1,7), activation='relu')(conv2D_5)
 conv2D_6 = Convolution2D(32, kernel_size=(1,1), activation='relu')(maxPool_1)
+conv2D_7 = Convolution2D(32, kernel_size=(3,3), activation='relu')(conv2D_5_2)
 
 flat_1 = Flatten()(conv2D_1)
 flat_2 = Flatten()(conv2D_4)
-flat_3 = Flatten()(conv2D_5)
+flat_3 = Flatten()(conv2D_7)
 flat_4 = Flatten()(conv2D_6)
 
-concatenate_1 = Concatenate()([flat_1, flat_2, flat_3, flat_4])
+
+concatenate_1 = Concatenate()([flat_2, flat_3, flat_4])
 
 dropout_1 = Dropout(40, noise_shape=None, seed=None)(concatenate_1)
 
 outputLayer = Dense(1, activation='softmax')(dropout_1)
+'''
+
+inputLayer = Input(shape=(IMG_SIZE,IMG_SIZE,3))
+
+conv2D_1 = Convolution2D(32, kernel_size=(3,3), strides=2)(inputLayer)
+conv2D_2 = Convolution2D(32, kernel_size=(3,3))(conv2D_1)
+conv2D_3 = Convolution2D(64, kernel_size=(3,3), padding='same')(conv2D_2)
+
+conv2D_4 = Convolution2D(96, kernel_size=(3,3), strides=2)(conv2D_3)
+maxPool_1 = MaxPooling2D(pool_size=(3,3), strides=2)(conv2D_3)
+
+concat_1 = Concatenate()([conv2D_4, maxPool_1])
+
+conv2D_5 = Convolution2D(64, kernel_size=(1,1), padding='same')(concat_1)
+conv2D_6 = Convolution2D(64, kernel_size=(7,1), padding='same')(conv2D_5)
+conv2D_7 = Convolution2D(64, kernel_size=(1,7), padding='same')(conv2D_6)
+conv2D_8 = Convolution2D(96, kernel_size=(3,3))(conv2D_7)
+
+conv2D_9 = Convolution2D(64, kernel_size=(1,1), padding='same')(concat_1)
+conv2D_10 = Convolution2D(96, kernel_size=(3,3))(conv2D_9)
+
+
+concat_2 = Concatenate()([conv2D_10, conv2D_8])
+
+
+conv2D_11 = Convolution2D(192, kernel_size=(3,3), strides=2)(concat_2)
+maxPool_2 = MaxPooling2D(pool_size=(3,3),strides=2)(concat_2)
+
+
+concat_3 = Concatenate()([conv2D_11,maxPool_2])
+
+flat_1 = Flatten()(concat_3)
+
+dropout_1 = Dropout(40)(flat_1)
+
+outputLayer = Dense(1, activation='softmax')(dropout_1)
+
 
 model = Model(inputLayer, outputLayer)
 
 model.summary()
 model.compile(loss='binary_crossentropy', optimizer='adam')
 model.fit_generator(generator=trainGen,
-					epochs=10,
+					epochs=20,
 					steps_per_epoch=20,
 					validation_steps=5,
                     validation_data=valGen)
-model.save("model1.h5")
+plot_model(model, to_file='model1_functional.png')
+model.save("model1_functional.h5")
 model.evaluate_generator(valGen, steps=5)
